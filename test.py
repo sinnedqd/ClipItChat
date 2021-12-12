6465a6355b2a372a8e976d89321f66caf43ca22e
dennisdang17 <dennisdang17@gmail.com>
2021-12-12 14:45:28 -0800

    Finished scoring algorithm

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import os
+import string
 import pandas as pd
 
 def pickle_to_csv(pathname, outfile):
@@ -12,12 +13,14 @@ def pickle_to_csv(pathname, outfile):
 
 def score_csv(pathname, outfile):
     df = pd.read_csv(pathname)
+    print(df)
     df.drop('Unnamed: 0', inplace=True, axis=1)
+    df.dropna(inplace=True)
 
     keywords_doc = open("keywords.txt", "r")
     keywords ={}
     for line in keywords_doc:
-        line = line.replace("\n","")
+        line = line.replace("\n","").lower()
         split_line = line.split(",")
         keywords[split_line[0]] = float(split_line[1])
     print(keywords)
@@ -25,6 +28,7 @@ def score_csv(pathname, outfile):
     score = 0.0
     scores = []
     for message in df.body:
+        print(message)
         message_arr = message.split()
         high_engagement = 0.0
         word_count = 0.0
@@ -32,8 +36,13 @@ def score_csv(pathname, outfile):
             word_count += 1.0
             if word.isupper():
                 high_engagement += .5
-            if word in keywords:
-                high_engagement += keywords[word]
+            if word.lower() in keywords:
+                if word.isupper():
+                    high_engagement += .5
+                high_engagement += keywords[word.lower()]
+            for letter in word:
+                if letter == "?" or letter =="!":
+                    high_engagement += 1.0
         score = high_engagement / word_count
         if(score > 1.0):
             score = 1.0
@@ -47,21 +56,17 @@ def score_csv(pathname, outfile):
     df.to_csv(outfile)
 
 def main():
-
     data_dir = "data"
     outfile = "finished"
     score_dir = "scored"
-    for filename in os.listdir(data_dir):
-        filepath = os.path.join(data_dir, filename)
-        outpath = os.path.join(outfile, filename)
-        pickle_to_csv(filepath, outpath)
-
+    # for filename in os.listdir(data_dir):
+    #     filepath = os.path.join(data_dir, filename)
+    #     outpath = os.path.join(outfile, filename)
+    #     pickle_to_csv(filepath, outpath)
 
     for filename in os.listdir(outfile):
         filepath = os.path.join(outfile, filename)
         outfile_path = os.path.join(score_dir,filename)
         score_csv(filepath, outfile_path)
     
-
-
 main()
