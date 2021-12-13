f1a6cb6c767bb6ec191cd8fdd113fd0d124766b4
Omar Hussain <omardhussain@gmail.com>
2021-12-12 23:15:08 -0800

    Updated scripting file

--- test2.py
+++ test2.py
@@ -1,13 +1,15 @@
 import os
 import glob
 import pandas as pd
-os.chdir("/Users/kevin/Documents/ClipItChat/finished")
+os.chdir("C:\\Users\\omar_\\Documents\\Code\\School\\CS_486\\CS-486\\Projects\\ClipItChat\\scored")
+
 
 extension = 'csv'
 all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
 
 #combine all files in the list
 combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
+
 #export to csv
-os.chdir("/Users/kevin/Documents/ClipItChat")
+os.chdir("C:\\Users\\omar_\\Documents\\Code\\School\\CS_486\\CS-486\\Projects\\ClipItChat")
 combined_csv.to_csv( "combined_csv.csv", index=False, encoding='utf-8-sig')
